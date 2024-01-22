f3a6c891477f0f1f2571889e675967bd60db1b7f
Kartikesh Mishra <mishrakesh3.14kartik@gmail.com>
2024-01-22 03:00:52 -0500

    designed new theme

---
+++ docs copy/resume.md
@@ -0,0 +1,54 @@
+# KARTIKESH MISHRA
+
+• Cambridge, MA 02139     	 
+• (+1) 617-642-9507  	
+• mk314k@mit.edu		 
+• linkedin.com/in/mk314k 	 
+• github.com/mk314k
+
+## Education
+### Massachusetts Institute of Technology 									Cambridge, MA
+#### Bachelor of Engineering in Computer Science & Engineering 							Class of 2024
+#### Bachelor of Science in Mathematics 										Class of 2024
+##### Relevant Courses: Machine Learning, Design and Analysis of Algorithms, Foundation of Statistics, Elements of Software Construction, Computational Structures, Fundamentals of Programming, Real Analysis
+#### Graduate Courses: Computer Vision, Advanced Computational Photography, Large Language Models, Matrix Methods
+## Technical Skills
+### Programming/Machine Learning: Python, C++, Typescript, Tensorflow, Pytorch, Transformer Lens, NumPy, OpenCV, Scikit-Learn
+### Web Development/Full Stack: HTML, CSS, Javascript, React, Node.js, Express.js, SQL
+### Hardware/Developer Tools: Arduino, ESP32, Jetson Tx2, Proteus, ROS, Linux, Bash, Git, VS Code, Vim
+## Relevant Experiences
+### Machine Learning Research Intern									June 2022 - August 2022
+### Open Learning Exchange Nepal, MISTI MIT								Kathmandu, Nepal
+Developed a pdf parser model to detect and classify text, images, and tables using OpenCV; tested on 200 pdf pages with 99% accuracy
+Created custom data structure for document pages using underlying geometric bounds to speed up the parser model by 10 times
+Designed a deep learning architecture, Convolutional Recurrent Neural Network using TensorFlow for Devanagari and English text recognition
+ML Lab Assistant, Intro to Machine Learning							January 2022 - May 2022
+Electrical Engineering and Computer Science (EECS) Department, MIT					Cambridge, MA
+Supervised a collaborative group session of 50 students in weekly interactive ML labs and examined their lab assignment progress
+Held weekly office hours to help students understand ML content such as neural networks, decision trees, reinforcement learning, and more
+Undergraduate Research Assistant									 June 2021 - Dec 2021
+Computer Science and Artificial Intelligence Laboratory (CSAIL), MIT					Cambridge, MA
+Developed a ROS program to auto-collect data from various sensors (including camera) for DJI Matrix 300 drones
+Built a ROS framework in a team of 3 to allow a single code run in both Flightmare Simulator and physical drone
+Developed drone autonomy software for autonomous flight with velocity control and path planning
+Projects
+Inpainting | C++, Python
+Implemented conjugate gradient descent (CGD) algorithm for deblurring images; solved poison equations and used CGD for image composition (overlaying an image over another). See the source on GitHub
+EQT | Typescript, Figma, node.js, express.js, HTML, CSS, SQL
+Developed a full-stack education platform to match students with tutors; designed a mathematical model to balance resources for students with diverse economic statuses. See the source on GitHub
+ReKnew ( Projecting Past Memories on Real Life Objects) | Python, OpenCV, MediaPipe
+Designed an algorithm using OpenCV and MediaPipe to detect targeted objects through the camera and augment our memories image on it
+R3DVAE-GAN | Python, Pytorch, VAE, GAN, Attention
+Augmented attention blocks in variational autoencoders and combined them with a generative adversarial network; trained them on dataset ShapeNetCore to learn 3d shapes from a single 2d image. See GitHub.
+Chess | Python, C++, Arduino, SQLite, HTTP
+A server-based chess engine allows two users across the globe to play using Arduino controlled Lcd display, taking inputs through buttons
+Extracurriculars
+MITeri Club MIT - WebMaster
+Manage the website by  updating events,  building a new interactive website, and fostering the Nepalese community at MIT
+Momentum AI MIT - Executive Member 
+Instructed Machine Learning with Python to build an end-to-end model over a period of two weeks for high school students in Boston
+Developed (in a team of 6) a machine learning course curriculum for high school students
+Awards
+Honorable mention - International Mathematics Olympiad (first from my country Nepal)
+3rd Best Project - Samsung Future of Wellness Hackathon 2022 (Project: ReKnew )
+Runner up - Easy A x Polkadot Web3 Hackathon 2023 (Project: Greenify)
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 import { Resume } from './resume';
@@ -31,16 +32,17 @@ function Header() {
     <>
       <header>
         <nav>
-          <div className="logo"></div>
+          {/* <div className="logo"></div>
+          <img src='../assets/logo.png' alt='Logo'></img> */}
           <div>
             <Link to="/">
-              <button className="nav-link">Home</button>
+              <span className="nav-link">About</span>
             </Link>
             <Link to="/project">
-              <button className="nav-link">Projects</button>
+              <span className="nav-link">Projects</span>
             </Link>
-            <Link to="/games">
-              <button className ="nav-link">Play Games</button>
+            <Link to="/resume">
+              <span className ="nav-link">Resume</span>
             </Link>
           </div>
         </nav>
@@ -49,17 +51,91 @@ function Header() {
   );
 }
 function Home(){
+  const [isExpanded, setExpanded] = useState(true);
+  const toggleExpand = () => {
+    setExpanded(!isExpanded);
+  };
   return (
     <>
       <div className="container flex-vertical">
-          <h2 className="name">Kartikesh Mishra</h2>
-          <p className="subtitle">| Student | Researcher | Developer | Designer |</p>
+          <h2 className="section-title">About me</h2>
+          <p className="about">
+            Hi! I'm Kartikesh, an undergraduate student at MIT, pursuing a dual major in Computer Science & Engineering and Mathematics (Class of 2024). 
+            My academic journey at MIT has been a dynamic exploration, delving into the realms of Machine Learning (ML), Analysis, Probability, and 
+            Statistics on the technical side, and Philosophy on the non-technical side. During my time at MIT, I've actively engaged in cutting-edge 
+            research, particularly focusing on Robotics and ML, with a keen interest in the interpretability of Large Language Models. 
+          </p><br />
+          {
+            isExpanded ? (
+              <>
+                <p className="about">
+                  Beyond the academic sphere, I contribute to the Momentum Ai club, where we empower underprivileged high school students in the Boston Area 
+                  with knowledge in AI and ML. I'm also a member of MITeri, a club fostering the Nepali community and those fascinated by Nepali cultures. 
+                </p><br />
+                <p className="about">
+                  In my leisure, I channel my passion for AI/ML and general Software Development into intriguing mini projects, many of which you can explore 
+                  on this website. Among my recent projects is "Pyssemble," a fusion of Python and assembly to simplify debugging for those learning Assembly. 
+                  Another project of mine is "R3DVAE-GAN," where I blend Attention in transformers with VAE-GAN architecture to facilitate the generation of 
+                  3D images from 2D counterparts.
+                </p><br />
+                <p className="about">
+                  My project interests span Education Technology, Green Environment initiatives, AI Safety projects leveraging ML, Full Stack Software development,
+                  and the fascinating world of Web3. If any of these areas resonate with you or spark curiosity, feel free to reach out. Whether you're looking to 
+                  collaborate, work together, or simply brainstorm, I'm always open to exciting opportunities. Let's connect and explore the realms of innovation together!
+                  {/* Hi, I am Kartikesh, an undergraduate student at MIT, majoring in Computer Science & Engineering and Mathematics (Class of 2024). At MIT,
+                  I have mostly taken classes focused on Machine Learning(ML), Analysis, Probability and Statisticsin the technical side and Philosophy on
+                  non-technical side. I have also been part of research focused on Robotics, ML especially Interpretability of Large Language Models. Apart 
+                  from research and academics, I am part of Momentum Ai club where, we teach AI and ML to underprivilleged high school students around Boston 
+                  Area. I am also part of MITeri, a club for Nepali community and people interested in Nepali cultures. 
+                  
+                  In my free time, I work on some mini projects in the AI/ML or general Software Development field, mostly listed on this website. Some of my
+                  recent projects include Pyssemble, where I am mixing python and assembly so as to help one with easy debugging when learning Assembly, and
+                  R3DVAE-GAN which combines Attention in transformer with VAE-GAN architecture for learning to generate 3d image from 2d images. Project-wise,
+                  I am interested in Education Technology, Green Environment, AI Safety related projects using the technology ML, Full Stack Software development
+                  and Web3. Please reach out to me if any of these sounds interesting and you want to work on it or simply brainstrom together. */}
+                </p>
+              </>
+            ):(
+              <p className="more-button" onClick={toggleExpand}>
+                Read More
+              </p>
+            )
+          }
+          
+          <br></br>
+          <br></br>
+      </div>
+      <div></div>
+      <Portfolio username="mk314k"/>
+    </>
+  );
+}
+function SideBar(){
+  return (
+    <>
+      <div className="sidebar flex-vertical">
+        <div className="flex-vertical">
+          <div className='fullname flex-vertical'>
+            <h2 className="name">Kartikesh</h2>
+            <h2 className="name">Mishra</h2>
+          </div>
+          <br />
+          <p className="subtitle">MIT Class of 2024</p>
+          <p className="subtitle">Comp. Sci. and Eng.</p>
+          <p className="subtitle">Mathematics</p>
+        </div>
+
+        <div className='flex-vertical'>
+          <br />
+          <p className='subtitle'>Software Developer</p>
+          <p className='subtitle'>ML Researcher</p>
           <SocialIcon />
           <p></p>
           <Link to="/resume">
             <button className="resume">View my Resume</button>
           </Link>
         </div>
+      </div>
     </>
   );
 }
@@ -69,13 +145,16 @@ function App() {
   return (
     <Router>
       <Header />
-      <main className="flex-vertical">
-        <Routes>
-          <Route path="/" element={<Home></Home>} />
-          <Route path="/resume" element={<Resume></Resume>} />
-          <Route path="/project" element={Portfolio({username:"mk314k"})} />
-          <Route path="/games" element={Games({username:"mk314k"})} />
-        </Routes>
+      <SideBar />
+      <main className='flex-vertical'>
+        {/* <div className='flex-vertical main-content'> */}
+          <Routes>
+            <Route path="/" element={<Home></Home>} />
+            <Route path="/resume" element={<Resume></Resume>} />
+            <Route path="/project" element={Portfolio({username:"mk314k"})} />
+            <Route path="/games" element={Games({username:"mk314k"})} />
+          </Routes>
+        {/* </div> */}
       </main>
       <footer>
 
