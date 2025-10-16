82f582685db2b6127757b6c02c5eb14f7a320d49
mk314k <mishrakesh3.14kartik@gmail.com>
2025-10-15 23:40:08 -0700

    updating about section

--- src/components/social_icons.tsx
+++ src/components/social_icons.tsx
@@ -1,18 +1,16 @@
-
-export const links = new Map([
-    ['github','https://github.com/mk314k'],
-    ['facebook', 'https://www.facebook.com/mk3.14k/'],
-    ['envelope', 'mailto:mk314k@mit.edu'],
-    ['linkedin', 'https://www.linkedin.com/in/mk314k/']
-  ]);
-  
 export function SocialIcon(){
+    const links = new Map<string, string>([
+      ['github','https://github.com/mk314k'],
+      ['facebook', 'https://www.facebook.com/mk3.14k/'],
+      ['envelope', 'mailto:mk314k@mit.edu'],
+      ['linkedin', 'https://www.linkedin.com/in/mk314k/']
+    ]);
     return (
       <>
         <div className="social-icons" id ="social-icons">
           {Array.from(links.keys()).map((key)=>(
             <div className='icon' key={key}>
-              <a href={links.get(key)} target='_blank'>
+              <a href={links.get(key)} target='_blank' rel="noopener noreferrer">
                 <i className={`fa${key==='envelope'?'':'-brands'} fa-${key}`}></i>
               </a>
             </div>
